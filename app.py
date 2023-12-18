5286d5952556fe61fe93ad4d60990ddeb52862dd
Marcin Grelak <s21258@pjwstk.edu.pl>
2023-12-18 23:42:04 +0100

    preparation for diamonds model

--- app.py
+++ app.py
@@ -6,41 +6,37 @@ from datetime import datetime
 startTime = datetime.now()
 # import znanych nam bibliotek
 
-filename = "model.h5"
-model = pickle.load(open(filename, 'rb'))
+filename = "x"
+# model = pickle.load(open(filename, 'rb'))
 # otwieramy wcześniej wytrenowany model
 
-sex_d = {0: "Kobieta", 1: "Mężczyzna"}
-pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
-embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
-
-
-# o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
 def main():
-    st.set_page_config(page_title="Czy przeżyłbyś katastrofę?")
+    st.set_page_config(page_title="Koszt Diamentu?")
     overview = st.container()
     left, right = st.columns(2)
     prediction = st.container()
 
     st.image(
-        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
+        "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ab/Diamond_JE3_BE3.png/revision/latest?cb=20230924193138"
+    )
 
     with overview:
-        st.title("Czy przeżyłbyś katastrofę?")
+        st.title("Ile te diaxy mają kosztować?")
 
     with left:
-        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
-        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
-                                  format_func=lambda x: embarked_d[x])
+        carat_slider = st.slider("Karaty", value=100, min_value=0, max_value=200)
+        cut_slider = st.slider("Cięcie", min_value=0, max_value=17)
+        color_slider = st.slider("Kolor", min_value=0, max_value=6)
+        clarity_slider = st.slider("Przejrzystość", min_value=0, max_value=10, step=10, value=0)
 
     with right:
-        age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
-        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
-        parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-        fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
+        depth_slider = st.slider("Głębia", min_value=0, max_value=500, step=10)
+        table_slider = st.slider("Tabela", min_value=0, max_value=500, step=10)
+        x_slider = st.slider("x", min_value=0, max_value=500, step=10)
+        y_slider = st.slider("y", min_value=0, max_value=500, step=10)
+        z_slider = st.slider("z", min_value=0, max_value=500, step=10)
 
-    data = np.array([1,pclass_radio,age_slider,sibsp_slider,parch_slider,fare_slider,embarked_radio,sex_radio])
+    data = np.array([])
     data = data.reshape(1,-1)
     #    data = np.array([])
     #    data = data.reshape(-1,1)
@@ -48,11 +44,9 @@ def main():
     s_confidence = model.predict_proba(data)
 
     with prediction:
-        st.header("Czy dana osoba przeżyje? {0}".format("Tak" if survival[0] == 1 else "Nie"))
+        st.header("sugerowany koszt diamentu to: {0}".format("Tak" if survival[0] == 1 else "Nie"))
         st.subheader("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
 
 if __name__ == "__main__":
     main()
-
-## Źródło danych [https://www.kaggle.com/c/titanic/](https://www.kaggle.com/c/titanic), zastosowanie przez Adama Ramblinga
