03f208128fc6ed46ee8d35fff95bd74e023e600d
Marcin Grelak <s21258@pjwstk.edu.pl>
2023-12-19 09:12:44 +0100

    dodano dane kategoryczne

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ model = ""
 # model = pickle.load(open(filename, 'rb'))
 # otwieramy wcześniej wytrenowany model
 
+color_d = {0: "D", 1: "E",2: "F", 3: "G",4: "H", 5: "I"}
+cut_d = {0: "Ideal", 1: "Premium",2: "Very Good", 3: "Good",4: "Fair"}
+clarity_d = {0: "SI1", 1: "SI2",2: "VS1", 3: "VS2",4: "VVS1", 5: "VVS2", 6: "I1", 7: "IF"}
 def main():
     st.set_page_config(page_title="Koszt Diamentu?")
     overview = st.container()
@@ -25,11 +28,10 @@ def main():
         st.title("Ile te diaxy mają kosztować?")
 
     with left:
+        cut_radio = st.radio("Cięcie", list(cut_d.keys()), index=0, format_func=lambda x:cut_d[x])
+        color_slider = st.radio("Kolor", list(color_d.keys()), index=1, format_func=lambda x:color_d[x])
+        clarity_slider = st.radio("Przejrzystość", list(clarity_d.keys()), index=2, format_func=lambda x:clarity_d[x])
         carat_slider = st.slider("Karaty", value=100, min_value=0, max_value=200)
-        cut_slider = st.slider("Cięcie", min_value=0, max_value=17)
-        color_slider = st.slider("Kolor", min_value=0, max_value=6)
-        clarity_slider = st.slider("Przejrzystość", min_value=0, max_value=10, step=10, value=0)
-
     with right:
         depth_slider = st.slider("Głębia", min_value=0, max_value=500, step=10)
         table_slider = st.slider("Tabela", min_value=0, max_value=500, step=10)
